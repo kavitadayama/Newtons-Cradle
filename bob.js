d176af7b355c7e0ee35283954b0cc389cb269abd
kavitadayama <70000736+kavitadayama@users.noreply.github.com>
2020-12-03 08:39:16 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x,y,radius){
+        var options = {
+            isStatic:false,
+            restitution: 1,
+            friction:0,
+            density:0.8
+        }
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.body = Bodies.circle(this.x,this.y,this.radius/2,options);
+        World.add(world,this.body)
+
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        strokeWeight(3);
+        fill("red");
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+    }
+     
+}
